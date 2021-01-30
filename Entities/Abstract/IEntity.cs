c8c671544342b2b87b00e41a3dd3a9a8602f78c5
Yunus Emre Cebe <yunusemrecebe@gmail.com>
2021-01-31 00:01:30 +0300

    Console Test Application

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Bu Arayüzü Kullanan Sınıflar Bir Veritabanı Tablosudur.
+
+    public interface IEntity
+    {
+    }
+}
