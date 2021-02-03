66b74c279a20b069c8a5d1b6451419d0e5281c07
Yunus Emre Cebe <yunusemrecebe@gmail.com>
2021-02-03 23:47:24 +0300

    EntityFramework İmpelementation

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -5,14 +5,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IProductDal
+    public interface IProductDal : IEntityRepository<Product>
     {
-        List<Product> GetAll();
-
-        void Add(Product product);
-        void Update(Product product);
-        void Delete(Product product);
-
-        List<Product> GetByCategory(int categoryId);
     }
 }
